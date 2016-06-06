e4c2604d8ee0119fa65dc24ead8fa3630896395e
unknown <wx@3.snail.com>
2016-06-06 13:59:17 +0800

    first commit

---
+++ DbUtils.py
@@ -0,0 +1,14 @@
+#encoding:utf-8
+from django.db import connection
+import MySQLdb
+from releaseinfo import *
+
+def cursor():
+    cursor = None
+    con = None
+    if DATABASE_ENGINE == 'sqlite3':
+        cursor = connection.cursor()
+    elif DATABASE_ENGINE == 'django.db.backends.mysql':
+        con = MySQLdb.connect(user=DATABASE_USER, db=DATABASE_NAME, passwd=DATABASE_PASSWORD, host=DATABASE_HOST, charset='utf8')
+        cursor = con.cursor()
+    return cursor, con
